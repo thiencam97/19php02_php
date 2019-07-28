a0ea55949678ebddc2d181a78e941eff9bf0fbf8
hungbuiqb16 <hungbuiqb16@gmail.com>
2019-07-28 16:49:07 +0700

    update

--- session24/exrcise/login.php
+++ session24/exrcise/login.php
@@ -5,17 +5,18 @@
   if(isset($_POST['submit'])) {
   	 $username = $_POST['username'];
   	 $password = $_POST['password'];
-      if($username == "admin" && $password == "123456") {
-      	   //lưu session
-		  	$_SESSION['username'] = $_POST['username'];
-		  	$_SESSION['password'] = $_POST['password'];   
-            header('Location:home.php');
-
-		  	
-     } else {
-           $info = "Đăng nhập thất bại";
+     if($username == "" || $username != "admin") {
+     	$errUsername = "Bạn chưa nhập hoặc nhập sai tên đăng nhập";
      }
-   }
+     if($password == "" || $password != "123456") {
+        $errPass = "Bạn chưa nhập hoặc nhập sai mật khẩu";
+     }
+     if ($username == "admin" && $password =="123456") {
+     	$_SESSION['username'] = $username;
+     	$_SESSION['pasword'] = $password;
+     	header("Location:home.php");
+     }
+     }
 ?>
 
 <!DOCTYPE html>
@@ -23,16 +24,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login | PHP</title>
+	<style>
+		.error{
+			color: red;
+		}
+	</style>
 </head>
 <body>
 	<h1>Login</h1>
 	<form action="#" method="post">
 		<label>Username</label><br>
-		<input type="text" name="username"><br>
-		<span class="error"></span>
+		<input type="text" name="username" value="<?php echo $username?>"><br>
+		<span class="error"><?php echo $errUsername?></span><br>
 		<label>Password</label><br>
 		<input type="password" name="password"><br>
-		<span class="error"></span>
+		<span class="error"><?php echo $errPass?></span><br>
 		<input type="submit" name="submit" value="Login"><br>
 		<span class="success"><?php echo $info;?></span>
 	</form>
